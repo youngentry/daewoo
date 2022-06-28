dbfeae23894e04557fc13eedd1654f3f6e99862e
youngentry <90388461+youngentry@users.noreply.github.com>
2022-06-28 23:51:05 +0900

    220628 직접시안코딩
    https://sakuraop.tistory.com/146

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(function () {
   $(".slider").slick({
-    slidesToShow: 3,
     centerMode: true,
+    slidesToShow: 3,
+  });
+
+  $(".toTop").on("click", function () {
+    $("html").animate({ scrollTop: 0 }, 200);
+  });
+
+  $(window).on("scroll", function () {
+    var screenHeight = $(window).scrollTop();
+    var btn = $(".toTop");
+    screenHeight > 700 ? btn.fadeIn(300) : btn.fadeOut(300);
   });
 });
