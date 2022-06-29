ebb363357837ebd013859a20cf88fd2a74821257
youngentry <gentry_@naver.com>
2022-06-29 14:46:47 +0900

    220629 대우코딩

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(function () {
   $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
     centerMode: true,
     slidesToShow: 3,
+    pauseOnHover: false,
+  });
+
+  $(".nav_right i").on("click", function () {
+    $(".nav_right input").toggle("on");
   });
 
   $(".toTop").on("click", function () {
@@ -12,5 +19,22 @@ $(function () {
     var screenHeight = $(window).scrollTop();
     var btn = $(".toTop");
     screenHeight > 700 ? btn.fadeIn(300) : btn.fadeOut(300);
+
+    if (screenHeight >= 600 && screenHeight < 1200) {
+      $(".content01 li").addClass("on");
+    }
+
+    if (screenHeight >= 1300 && screenHeight < 1700) {
+      $(".content_box01").addClass("on");
+    }
+    if (screenHeight >= 1700 && screenHeight < 2200) {
+      $(".content_box02").addClass("on");
+    }
+    if (screenHeight >= 2100 && screenHeight < 2500) {
+      $(".content_box03").addClass("on");
+    }
+    if (screenHeight >= 2600 && screenHeight < 3300) {
+      $(".content_box04").addClass("on");
+    }
   });
 });
